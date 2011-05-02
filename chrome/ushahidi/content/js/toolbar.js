bed78d0545493df07a6b958b009a27878cb47639
David Kobia <david@ushahidi.com>
2011-05-02 08:51:08 -0400

    * Initial Commit

---
+++ chrome/ushahidi/content/js/toolbar.js
@@ -0,0 +1,15 @@
+/**
+ * LICENSE: This source file is subject to the GPL license 
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/licenses/gpl.html
+ * @author     Ushahidi Team <team@ushahidi.com> 
+ * @copyright  Ushahidi - http://www.ushahidi.com
+ * @license    http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL) 
+ */
+
+window.addEventListener("load", UshahidiToolbar.Init, false);
+window.addEventListener("unload", UshahidiToolbar.Exit, false);
+
+var UshahidiToolbar = {
+	
+}; // End of UshahidiToolbar
